e42491cedcb23bc74fbd499bff53e596bda3d741
Marcio Mazza <marciomazza@gmail.com>
2020-06-05 17:32:07 +0200

    Configure flake8

--- codesummary/__init__.py
+++ codesummary/__init__.py
@@ -4,5 +4,5 @@ __author__ = """Marcio Mazza"""
 __email__ = "marciomazza@gmail.com"
 __version__ = "0.1.0"
 
-from .codesummary import get_stores_and_loads, summarize
-from .ipython_extension import load_ipython_extension
+from .codesummary import get_stores_and_loads, summarize  # noqa: F401
+from .ipython_extension import load_ipython_extension  # noqa: F401
